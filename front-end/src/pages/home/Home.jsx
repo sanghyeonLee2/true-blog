50c5c2adb77efe493bf9701aa3882c1bf93e5a1e
sanghyeonLee2 <hyeon012366@gmail.com>
2024-05-07 18:46:09 +0900

    Refactor : 전역 스타일 파일 결합에 따른 중복코드 리펙토링

--- front-end/src/pages/home/Home.jsx
+++ front-end/src/pages/home/Home.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import {
-    SearchBoxContainer, CategoryNav, CategoryUl,
+    CategoryNav, CategoryUl,
     HomeContentsContainer,
     HomeContentsInnerContainer,
-    HomeMainContents, HomeMainContentsContainer, SearchInput, SearchIconSpan
+    HomeMainContents, HomeMainContentsContainer, SearchSectionContainer, SearchSectionOuterContainer,
 } from "./HomeStyle";
+import {SearchBoxContainer} from "../../style/ConstantStyle";
 
 function Home(props) {
     return (
         <>
-            <SearchBoxContainer>
-                    <SearchInput placeholder={"검색창"}/>
-                    <SearchIconSpan/>
-            </SearchBoxContainer>
+            <SearchSectionContainer>
+                <SearchBoxContainer>
+                    <input className={"search-input"} placeholder={"검색창"}/>
+                    <span className={"search-icon"}/>
+                </SearchBoxContainer>
+            </SearchSectionContainer>
             <HomeContentsContainer>
                 <HomeContentsInnerContainer>
                     <CategoryNav>
                         <CategoryUl>
                             <li>
-                                <h1>요소1</h1>
+                                <h1>트렌딩</h1>
                             </li>
                             <li>
-                                <h1>요소2</h1>
+                                <h1>최신</h1>
                             </li>
                             <li>
-                                <h1>요소3</h1>
-                            </li>
-                            <li>
-                                <h1>요소4</h1>
+                                <h1>피드</h1>
                             </li>
                         </CategoryUl>
                     </CategoryNav>
