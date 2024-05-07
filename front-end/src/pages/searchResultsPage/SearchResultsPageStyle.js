7b88d263d2b87bbc1b3370d80e8c81ee478a564f
sanghyeonLee2 <hyeon012366@gmail.com>
2024-05-07 12:07:16 +0900

    Design : 검색결과 컴포넌트 css 코드 작성

---
+++ front-end/src/pages/searchResultsPage/SearchResultsPageStyle.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const SearchResultsPageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+
+export const SearchResultsPageSearchBox = styled.div`
+  background-color: darkgray;
+  height: 120px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+export const SearchResultsContentsContainer= styled.div`
+  display: flex;
+  justify-content: space-around;
+  background-color: darkgoldenrod;
+  width: 80%;
+  `
+export const SearchResultContent = styled.div`
+  width: 33%;
+  height: 50px;
+  background-color: darkslateblue;
+`
