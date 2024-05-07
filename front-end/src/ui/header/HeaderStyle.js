a4ef23864d957a864e1956adbe3ac7cd0f3e2ec1
sanghyeonLee2 <hyeon012366@gmail.com>
2024-05-07 12:04:27 +0900

    Design : header 컴포넌트 css 작성

---
+++ front-end/src/ui/header/HeaderStyle.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 100px;
+`
+export const LeftHeaderNav = styled.nav`
+  display: flex;
+  float: left;
+  margin: 3px 40px;
+`
+export const HeaderSpan = styled.span`
+padding: 8px`
+export const RightHeaderNav = styled.nav`
+display: flex;
+float:right;
+  margin: 3px 40px;`
