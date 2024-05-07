3c4ac3e031f1d2b5675585385b4c918e6e7c7e28
sanghyeonLee2 <hyeon012366@gmail.com>
2024-05-07 12:05:08 +0900

    Design : Home.jsx 컴포넌트 css 적용

--- front-end/src/pages/home/Home.jsx
+++ front-end/src/pages/home/Home.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
+import {
+    SearchBoxContainer, CategoryNav, CategoryUl,
+    HomeContentsContainer,
+    HomeContentsInnerContainer,
+    HomeMainContents, HomeMainContentsContainer, SearchInput, SearchIconSpan
+} from "./HomeStyle";
 
 function Home(props) {
     return (
-        <div></div>
+        <>
+            <SearchBoxContainer>
+                    <SearchInput placeholder={"검색창"}/>
+                    <SearchIconSpan/>
+            </SearchBoxContainer>
+            <HomeContentsContainer>
+                <HomeContentsInnerContainer>
+                    <CategoryNav>
+                        <CategoryUl>
+                            <li>
+                                <h1>요소1</h1>
+                            </li>
+                            <li>
+                                <h1>요소2</h1>
+                            </li>
+                            <li>
+                                <h1>요소3</h1>
+                            </li>
+                            <li>
+                                <h1>요소4</h1>
+                            </li>
+                        </CategoryUl>
+                    </CategoryNav>
+                    <HomeMainContentsContainer>
+                        <HomeMainContents>
+                            sda
+                        </HomeMainContents>
+                    </HomeMainContentsContainer>
+                </HomeContentsInnerContainer>
+            </HomeContentsContainer>
+        </>
     );
 }
 
