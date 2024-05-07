a1c3193a0803b1c0b9882cbabd23062adc79d8da
sanghyeonLee2 <hyeon012366@gmail.com>
2024-05-07 12:03:48 +0900

    Design : Header.jsx 컴포넌트 css 적용

--- front-end/src/ui/header/Header.jsx
+++ front-end/src/ui/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import {HeaderContainer, HeaderSpan, LeftHeaderNav, RightHeaderNav} from "./HeaderStyle";
+
 
 function Header(props) {
-    return (
-        <div></div>
-    );
+    return (<HeaderContainer>
+        <LeftHeaderNav>
+                <HeaderSpan>
+                    <h1>True blog</h1>
+                </HeaderSpan>
+                <HeaderSpan>
+                 <h1>북마크</h1>
+                </HeaderSpan>
+        </LeftHeaderNav>
+        <RightHeaderNav>
+                <HeaderSpan>
+                <h1>로그인</h1>
+                    </HeaderSpan>
+            <HeaderSpan>
+                <h1>회원가입</h1>
+                    </HeaderSpan>
+        </RightHeaderNav>
+    </HeaderContainer>);
 }
 
 export default Header;
